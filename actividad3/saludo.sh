2bd66092a10d7de02f0dc65b9b448f6e062a1316
Jpanaza1906 <3848221850101@ingenieria.usac.edu.gt>
2024-02-21 23:20:49 -0600

    act3

---
+++ actividad3/saludo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Ciclo que muestre un saludo, fecha cada segundo
+
+while true
+do
+    echo "Saludos Amigo de la USAC, Te deseo exitos el dia de hoy $(date)"
+    sleep 1
+done
+
+#Tener presente la ruta donde se guardo /home/josep/USAC/7mo_Semestre/Sistemas_operativos_1/Actividades/so1_actividades_202111478/actividad3/saludo.sh
+
+#Darle permisos chmod +x /home/josep/USAC/7mo_Semestre/Sistemas_operativos_1/Actividades/so1_actividades_202111478/actividad3/saludo.sh
